de759e98bfb418ee91ab27f5a7dfe2fbfdb9dd47
rachit_iam <rachitrahul23@gmail.com>
2020-10-31 15:19:53 +0530

    Changed to correct password

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "1234",
     database: "test",
     multipleStatements: true
 });
