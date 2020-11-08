805a69eeec6840e579a77604e3d397ec568a7f40
rachit_iam <rachitrahul23@gmail.com>
2020-11-09 02:41:26 +0530

    ditched ck user and implemented own user model and controllers

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,13 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { register, Login,signout, userList, userEdit, validate ,checkToken} = require("../controllers/UserController");
-
-
+const { signup, login } = require("../controllers/UserController");
 
 //extra Routes
-router.post("/signin", Login);
-router.post("/signout", signout);
-router.post("/validate", validate);
-//router.get("/userlist", userList);
-router.get("/userEdit/:id", userEdit);
+router.post("/login", login);
+router.post("/signup", signup);
 module.exports = router;
